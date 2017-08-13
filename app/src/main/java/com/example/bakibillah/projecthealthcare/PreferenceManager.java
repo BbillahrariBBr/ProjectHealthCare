3ffaaf76bc66bbdb6c3dd24664b5af048b07c310
312soft <312soft@gmail.com>
2017-08-14 01:29:36 +0600

    AppIntro fullComplete

---
+++ app/src/main/java/com/example/bakibillah/projecthealthcare/PreferenceManager.java
@@ -0,0 +1,53 @@
+package com.example.bakibillah.projecthealthcare;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by BakiBillah on 8/14/2017.
+ *
+ * isnt add this
+ */
+
+public class PreferenceManager {
+
+    private Context context;
+    private SharedPreferences sharedPreferences;
+
+    //preferance manager is not add
+
+    public PreferenceManager(Context context){
+        this.context = context;
+        getSharedPrefernce();
+    }
+
+
+    private void getSharedPrefernce(){
+        sharedPreferences = context.getSharedPreferences(context.getString(R.string.my_preference),Context.MODE_PRIVATE);
+
+    }
+
+    public void writePreference(){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(context.getString(R.string.my_preference_key),"INIT_OK");
+        editor.commit();
+
+    }
+
+    public boolean checkPreference(){
+        boolean status = false;
+        if (sharedPreferences.getString(context.getString(R.string.my_preference_key),"null").equals("null")){
+            status = false;
+        }
+        else {
+            status = true;
+        }
+
+        return status;
+    }
+
+    public void clearPreference(){
+        sharedPreferences.edit().clear().commit();
+    }
+
+}
