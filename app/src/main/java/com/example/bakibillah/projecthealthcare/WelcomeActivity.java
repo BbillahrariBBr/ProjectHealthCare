db92193ccb2437a78dc076be44a0e6bab62cbf1e
312soft <312soft@gmail.com>
2017-08-14 01:17:49 +0600

    AppIntro fullComplete

--- app/src/main/java/com/example/bakibillah/projecthealthcare/WelcomeActivity.java
+++ app/src/main/java/com/example/bakibillah/projecthealthcare/WelcomeActivity.java
@@ -1,16 +1,19 @@
 package com.example.bakibillah.projecthealthcare;
 
+import android.content.Intent;
 import android.os.Build;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     ViewPager mPager; // view pager value
     private int[] layouts = {R.layout.firstslide,R.layout.secondslide, R.layout.thirdslide,R.layout.fourthslide,R.layout.fifthslide};
@@ -19,9 +22,16 @@ public class WelcomeActivity extends AppCompatActivity {
 
     private LinearLayout Dots_layout;
     private ImageView[] dots;
+    private Button BnNext;
+    private Button BnSkip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+        if(new PreferenceManager(this).checkPreference()){
+            loadHome();
+        }
 
         if(Build.VERSION.SDK_INT>=19){
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
@@ -38,6 +48,11 @@ public class WelcomeActivity extends AppCompatActivity {
         mPager.setAdapter(mpagerAdapter);
 
         Dots_layout =(LinearLayout)findViewById(R.id.dotslayout);
+        BnNext = (Button)findViewById(R.id.bnnext);
+        BnSkip = (Button)findViewById(R.id.bnskip);
+        BnNext.setOnClickListener(this);
+        BnSkip.setOnClickListener(this);
+
 
         createDots(0);
 
@@ -49,7 +64,18 @@ public class WelcomeActivity extends AppCompatActivity {
 
             @Override
             public void onPageSelected(int position) {
+
                 createDots(position);
+
+                if(position==layouts.length-1){
+                    BnNext.setText("Start");
+                    BnSkip.setVisibility(View.INVISIBLE);
+                }
+
+                else {
+                    BnNext.setText("Next");
+                    BnSkip.setVisibility(View.VISIBLE);
+                }
             }
 
             @Override
@@ -86,5 +112,43 @@ public class WelcomeActivity extends AppCompatActivity {
             Dots_layout.addView(dots[i],params);
         }
 
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+            case R.id.bnnext:
+                loadNextSlide();
+                break;
+
+            case R.id.bnskip:
+                loadHome();
+                new PreferenceManager(this).writePreference();
+                break;
+        }
+
+    }
+    //for Main or homeActivity
+    private void loadHome(){
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
+    }
+
+    //handle next click
+
+    private void loadNextSlide(){
+
+        int next_slide = mPager.getCurrentItem()+1;
+
+        if (next_slide<layouts.length){
+            mPager.setCurrentItem(next_slide);
+        }
+        else {
+            loadHome();
+            new PreferenceManager(this).writePreference();
+            //new PreferenceManager(this).clearPreference();
+            //finish();
+        }
     }
 }
