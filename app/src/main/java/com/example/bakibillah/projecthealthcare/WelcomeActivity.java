e29d33a20673ad18639cda233183ca7f2d9e7d17
312soft <312soft@gmail.com>
2017-08-14 00:02:42 +0600

    AppIntro Step4 Active Dots

--- app/src/main/java/com/example/bakibillah/projecthealthcare/WelcomeActivity.java
+++ app/src/main/java/com/example/bakibillah/projecthealthcare/WelcomeActivity.java
@@ -1,17 +1,24 @@
 package com.example.bakibillah.projecthealthcare;
 
 import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class WelcomeActivity extends AppCompatActivity {
 
     ViewPager mPager; // view pager value
-    private int[] layout = {R.layout.firstslide,R.layout.secondslide, R.layout.thirdslide,R.layout.fourthslide,R.layout.fifthslide};
+    private int[] layouts = {R.layout.firstslide,R.layout.secondslide, R.layout.thirdslide,R.layout.fourthslide,R.layout.fifthslide};
     //layout resouese id taken
     private MpagerAdapter mpagerAdapter;
+
+    private LinearLayout Dots_layout;
+    private ImageView[] dots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +34,57 @@ public class WelcomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
 
         mPager = (ViewPager)findViewById(R.id.viewPager);
-        mpagerAdapter = new MpagerAdapter(layout,this);
+        mpagerAdapter = new MpagerAdapter(layouts,this);
         mPager.setAdapter(mpagerAdapter);
 
+        Dots_layout =(LinearLayout)findViewById(R.id.dotslayout);
+
+        createDots(0);
+
+        mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                createDots(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+
+    }
+
+    private void createDots(int current_postion){
+
+        if (Dots_layout!=null){
+            Dots_layout.removeAllViews();
+        }
+
+        dots = new ImageView[layouts.length];
+
+        for (int i = 0; i <layouts.length ; i++) {
+
+            dots[i] = new ImageView(this);
+
+            if (i ==current_postion){
+                dots[i].setImageDrawable(ContextCompat.getDrawable(this,R.drawable.active_dots));
+            }
+            else {
+                dots[i].setImageDrawable(ContextCompat.getDrawable(this,R.drawable.default_dots));
+            }
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT);
+            params.setMargins(4,0,4,0);
+            Dots_layout.addView(dots[i],params);
+        }
 
     }
 }
